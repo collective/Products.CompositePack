4b55df80316c8cca2b95b6e155210f44f46d6612
duncanb <duncanb>
2005-10-03 14:29:48 +0000

    Copied remotely

---
+++ build1.2/composite/__init__.py
@@ -0,0 +1,13 @@
+##############################################################################
+#
+# Copyright (c) 2004 CompositePack Contributors. All rights reserved.
+#
+# This software is distributed under the terms of the Zope Public
+# License (ZPL) v2.1. See COPYING.txt for more information.
+#
+##############################################################################
+"""This is a package.
+
+$Id$
+"""
+import archetype

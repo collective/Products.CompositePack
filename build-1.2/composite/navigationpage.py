afce634386010d7e5207942718e9cfe11e006032
duncanb <duncanb>
2005-10-12 11:57:23 +0000

    Replacing tag

---
+++ build-1.2/composite/navigationpage.py
@@ -0,0 +1,49 @@
+##############################################################################
+#
+# Copyright (c) 2004 CompositePack Contributors. All rights reserved.
+#
+# This software is distributed under the terms of the Zope Public
+# License (ZPL) v2.1. See COPYING.txt for more information.
+#
+##############################################################################
+"""Navigation Page :
+
+$Id$
+"""
+from Acquisition import aq_base
+
+from Products.Archetypes.public import BaseSchema
+
+from Products.CompositePack.config import PROJECTNAME
+from Products.CompositePack.composite import packcomposite
+from Products.CompositePack.public import BaseContent, registerType
+
+class NavigationPage(BaseContent):
+    """A basic, Archetypes-based Composite Page
+    """
+    meta_type = portal_type = 'Navigation Page'
+    archetype_name = 'Navigation Page'
+    schema = BaseSchema.copy()
+    # Move the description field into the edit view.
+    schema['description'].isMetadata = False
+    schema['description'].schemata = 'default'
+    actions = packcomposite.actions
+    
+    factory_type_information={
+            'content_icon':'composite.gif',
+            }
+
+    def SearchableText(self):
+        """Return text for indexing"""
+        # Want title, description, and all Title and fragment content.
+        # Fragments are converted from HTML to plain text.
+        texts = [self.title, self.description]
+        if getattr(aq_base(self.cp_container), 'titles', None) is not None:
+	    titles = self.cp_container.titles.objectValues()
+	    for o in titles:
+		if hasattr(o, 'ContainerSearchableText'):
+		    texts.append(o.ContainerSearchableText())
+
+        return " ".join(texts)
+
+registerType(NavigationPage, PROJECTNAME)
---
+++ build-1.2/config.py
@@ -0,0 +1,59 @@
+##############################################################################
+#
+# Copyright (c) 2004 CompositePack Contributors. All rights reserved.
+#
+# This software is distributed under the terms of the Zope Public
+# License (ZPL) v2.1. See COPYING.txt for more information.
+#
+##############################################################################
+"""Product configuration : constants
+
+$Id$
+"""
+import os
+import Globals
+import warnings
+
+try:
+    from Products import ATContentTypes
+    HAS_ATCT = True
+except ImportError:
+    HAS_ATCT = False
+
+try:
+    from Products.CMFPlone.migrations.v2_1 import rcs
+except ImportError:
+    PLONE21 = False
+else:
+    PLONE21 = True
+
+try:
+    from Products.azax import AzaxResponse
+    from Products import Five
+except ImportError:
+    warnings.warn('CompositePack performance impaired: Five and Azax recommended', DeprecationWarning)
+    HAVEAZAX = False
+else:
+    HAVEAZAX = True
+
+PROJECTNAME = 'CompositePack'
+ADD_CONTENT_PERMISSION = 'Add CompositePack content'
+GLOBALS = globals()
+
+TOOL_ID = 'composite_tool'
+TOOL_NAME = 'CompositePack Tool'
+TOOL_ICON = 'composite.gif'
+
+ATCT_TYPES = ['ATDocument', 'ATNewsItem', 'ATEvent', 'ATFile', 'ATImage',
+              'ATTopic', 'ATLink']
+MIGRATED_ATCT_TYPES = ['Document', 'News Item', 'Event', 'File', 'Image',
+                       'Topic', 'Link']
+COMPOSABLE = 'composable'
+
+VIEWLETS = 'viewlets'
+LAYOUTS = 'layouts'
+
+INSTALL_DEMO_TYPES = 0 ##Install the demo types
+
+zmi_dir = os.path.join(Globals.package_home(globals()),'www')
+

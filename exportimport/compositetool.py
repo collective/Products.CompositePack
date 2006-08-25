b2673a55837076f9827658d8005fe99041d8af28
Jean-Paul Ladage <j.ladage@zestsoftware.nl>
2006-08-25 19:41:12 +0000

    Added Changelog, credits and legal info.

--- exportimport/compositetool.py
+++ exportimport/compositetool.py
@@ -1,3 +1,15 @@
+##############################################################################
+#
+# Copyright (c) 2004-2006 CompositePack Contributors. All rights reserved.
+#
+# This software is distributed under the terms of the Zope Public
+# License (ZPL) v2.1. See COPYING.txt for more information.
+#
+##############################################################################
+"""
+$Id: compositetool.py 18879 2006-02-02 15:27:55Z jladage $
+"""
+
 from zope.app import zapi
 from Products.CMFCore.utils import getToolByName
 from Products.GenericSetup.interfaces import IBody
--- interfaces.py
+++ interfaces.py
@@ -1,6 +1,16 @@
+##############################################################################
+#
+# Copyright (c) 2004-2006 CompositePack Contributors. All rights reserved.
+#
+# This software is distributed under the terms of the Zope Public
+# License (ZPL) v2.1. See COPYING.txt for more information.
+#
+##############################################################################
+"""
+$Id: compositetool.py 18879 2006-02-02 15:27:55Z jladage $
+"""
 from zope.interface import Interface
 from zope.interface import Attribute
-
 #
 #   Composite tool interface
 #

aea19021f0b82b0c054216fe0740f9c91a526b0e
duncanb <duncanb>
2007-09-21 10:21:25 +0000

    Zope no longer needs patches to all of objectIds, objectValues and objectItems since the last two are implemented using the first one. This patch breaks snapshotting, so I've removed the unwanted hotfixes.

--- composite/packcomposite.py
+++ composite/packcomposite.py
@@ -453,20 +453,6 @@ def objectIds(self, spec=None):
     return ids
 objectIds.__cp_method__ = True
 
-def objectValues(self, spec=None):
-    objs = self.__cp_objectValues__(spec)
-    if check_spec(spec) and shasattr(self, 'cp_container'):
-        objs = list(objs) + [self.cp_container]
-    return objs
-objectValues.__cp_method__ = True
-
-def objectItems(self, spec=None):
-    items = self.__cp_objectItems__(spec)
-    if check_spec(spec) and shasattr(self, 'cp_container'):
-        items = list(items) + [('cp_container', self.cp_container)]
-    return items
-objectItems.__cp_method__ = True
-
 manage_composite_contents = PageTemplateFile('compositeContents.pt',
         zmi_dir)
 manage_composite_contents.__cp_method__ = True
@@ -480,5 +466,4 @@ methods = {'_setPortalTypeName':_setPortalTypeName,
            'manage_composite_contents':manage_composite_contents,
            '_notifyOfCopyTo':_notifyOfCopyTo,
            'objectIds':objectIds,
-           'objectValues':objectValues,
-           'objectItems':objectItems}
+       }
